57430d14f6417327cd899c669a1fbfea3fcdfd07
jessmelo <50722304+jessmelo@users.noreply.github.com>
2020-05-02 22:51:00 -0300

    retorna -1 se a entrada do método for incorreta

--- EP2/Raiz.java
+++ EP2/Raiz.java
@@ -1,3 +1,49 @@
-public class Raiz {
+/*********************************************************************/
+/**   ACH2001 - Introdução à Programação                            **/
+/**   EACH-USP - Primeiro Semestre de 2020                          **/
+/**   <turma> - <nome do professor>                                 **/
+/**                                                                 **/
+/**   Segundo Exercício-Programa                                    **/
+/**                                                                 **/
+/**   Jéssica da Paixão Melo               Nº USP: 10875986         **/
+/**                                                                 **/
+/**   <data de entrega>                                             **/
+/*********************************************************************/
 
+/*
+	Cálculo para raiz quadrada
+*/
+public class Raiz {
+	/*
+		Calcula a raiz quadrada de um valor, com uma determinada
+		precisão. Retorna esse valor, ou -1 quando:
+		
+		* a < 0
+		* epsilon <= 0
+		* epsilon >= 1
+		
+		Parâmetro:
+			a - valor cuja raiz quadrada será calculada
+			epsilon - precisão do cálculo
+	*/
+	static double raizQuadrada(double a, double epsilon) {
+        // escreva seu código aqui
+        if(a < 0 || epsilon >= 1 || epsilon <= 0){
+            return -1;
+        } 
+        return 0;
+	}
+
+
+	/*
+		Apenas para seus testes. ISSO SERÁ IGNORADO NA CORREÇÃO
+	*/
+	public static void main(String[] args) {
+		// escreva seu código (para testes) aqui
+
+		// Exemplo de teste:
+		double valor = 63;
+		double precisao = 0.001;
+		System.out.println("Raiz de : "+valor+" = "+raizQuadrada(valor, precisao));
+	}
 }

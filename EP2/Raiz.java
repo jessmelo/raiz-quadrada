927a15a58b22e6367534b1e7bf659354ff9f4d62
jessmelo <50722304+jessmelo@users.noreply.github.com>
2020-05-03 14:13:21 -0300

    final version

--- EP2/Raiz.java
+++ EP2/Raiz.java
@@ -1,22 +1,36 @@
 /*********************************************************************/
 /**   ACH2001 - Introdução à Programação                            **/
 /**   EACH-USP - Primeiro Semestre de 2020                          **/
-/**   <turma> - <nome do professor>                                 **/
+/**   Turma 04 - Norton Trevisan Roman                              **/
 /**                                                                 **/
 /**   Segundo Exercício-Programa                                    **/
 /**                                                                 **/
 /**   Jéssica da Paixão Melo               Nº USP: 10875986         **/
 /**                                                                 **/
-/**   <data de entrega>                                             **/
+/**   03/05/2020                                                    **/
 /*********************************************************************/
 
 /*
 	Cálculo para raiz quadrada
 */
 
-import java.lang.Math;
-
 public class Raiz {
+
+	/* 
+		Método que retorna o valor absoluto de um número
+	*/
+	
+	static double valorAbs (double num){
+		double abs;
+
+		if (num > 0){
+			abs = num;
+		} else {
+			abs = num * -1;
+		}
+		return abs;
+	}
+
 	/*
 		Calcula a raiz quadrada de um valor, com uma determinada
 		precisão. Retorna esse valor, ou -1 quando:
@@ -29,18 +43,27 @@ public class Raiz {
 			a - valor cuja raiz quadrada será calculada
 			epsilon - precisão do cálculo
 	*/
+
 	static double raizQuadrada(double a, double epsilon) {
         // escreva seu código aqui
-        if(a < 0 || epsilon >= 1 || epsilon <= 0){
-            return -1;
-        } 
-        double xi = a/2;
-        double abs = 0;
-        while(abs >= epsilon){
-            xi = 1/2 * (xi + (a / xi));
-            abs = Math.abs(xi + 1 - xi);
+		if(a < 0 || epsilon >= 1 || epsilon <= 0){
+        	return -1;
+		} 
+		
+        double xi = a / 2.0; // x0
+		double abs = 1.0;  // variavel que guarda valor absoluto
+
+		double raiz = 1.0; // variavel que guarda a raiz
+
+		while (abs >= epsilon){
+			raiz = xi;
+			double xAnt = xi;
+            xi = 1/2.0 * (xi + (a/xi));
+			double num = xAnt - xi;    
+			abs = valorAbs(num);
         }
-        return xi;
+		
+		return raiz;
 	}
 
 
@@ -54,5 +77,6 @@ public class Raiz {
 		double valor = 63;
 		double precisao = 0.001;
 		System.out.println("Raiz de : "+valor+" = "+raizQuadrada(valor, precisao));
+		
 	}
 }
